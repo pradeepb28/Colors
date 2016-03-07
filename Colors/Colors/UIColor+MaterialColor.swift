e903cb1748d806ac5d28cba62b44a849aae734af
pradeepb28 <pradeepboorgu@gmail.com>
2016-03-06 21:35:57 -0600

    Added more colors

--- Colors/Colors/UIColor+MaterialColor.swift
+++ Colors/Colors/UIColor+MaterialColor.swift
@@ -5,6 +5,25 @@
 //  Created by Pradeep Burugu on 3/6/16.
 //  Copyright © 2016 Pradeep Burugu. All rights reserved.
 //
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 
 import Foundation
 import UIKit
@@ -20,42 +39,67 @@ extension UIColor {
     class func materialPurpleColor(alpha:CGFloat = 1.0)->UIColor {
         return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(103,58,183)
     class func materialDeepPurpleColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 103.0/255.0, green: 58.0/255.0, blue: 183.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(63,81,181)
     class func materialIndigoColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 63.0/255.0, green: 81.0/255.0, blue: 181.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(33,150,243)
     class func materialBlueColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 33.0/255.0, green: 150.0/255.0, blue: 243.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(3,169,244)
     class func materialLightBlueColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 3.0/255.0, green: 169.0/255.0, blue: 244.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(0,188,212)
     class func materialCyanColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 0.0/255.0, green: 188.0/255.0, blue: 212.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(0,150,136)
     class func materialTealColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 0.0/255.0, green: 150.0/255.0, blue: 136.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(76,175,80)
     class func materialGreenColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 76.0/255.0, green: 175.0/255.0, blue: 80.0/255.0, alpha: alpha)
     }
-    //
+    //rgb(139,195,74)
     class func materialLightGreenColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
-    }//
+        return UIColor(red: 139.0/255.0, green: 195.0/255.0, blue: 74.0/255.0, alpha: alpha)
+    }//rgb(205,220,57)
     class func materialLimeColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
-    }//
+        return UIColor(red: 205.0/255.0, green: 220.0/255.0, blue: 57.0/255.0, alpha: alpha)
+    }//rgb(255,235,59)
     class func materialYellowColor(alpha:CGFloat = 1.0)->UIColor {
-        return UIColor(red: 156.0/255.0, green: 39.0/255.0, blue: 176.0/255.0, alpha: alpha)
+        return UIColor(red: 255.0/255.0, green: 235.0/255.0, blue: 59.0/255.0, alpha: alpha)
     }
+    //rgb(255,193,7)
+    class func materialAmberColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 255.0/255.0, green: 193.0/255.0, blue: 7.0/255.0, alpha: alpha)
+    }
+    //rgb(255,152,0)
+    class func materialOrangeColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 255.0/255.0, green: 152.0/255.0, blue: 0.0/255.0, alpha: alpha)
+    }
+    //rgb(255,87,34)
+    class func materialDeepOrangeColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 255.0/255.0, green: 87.0/255.0, blue: 34.0/255.0, alpha: alpha)
+    }
+    //rgb(121,85,72)
+    class func materialBrownColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 121.0/255.0, green: 85.0/255.0, blue: 72.0/255.0, alpha: alpha)
+    }
+    //rgb(158,158,158)
+    class func materialGreyColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 158.0/255.0, green: 158.0/255.0, blue: 158.0/255.0, alpha: alpha)
+    }
+    //rgb(96,125,139)
+    class func materialBlueGreyColor(alpha:CGFloat = 1.0)->UIColor {
+        return UIColor(red: 96.0/255.0, green: 125.0/255.0, blue: 139.0/255.0, alpha: alpha)
+    }
+    
 }
